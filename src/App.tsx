d37138b124435cf89cfba42e9a5165d99ebcfd93
so.ota <so.ota.biz@gmail.com>
2025-09-21 17:55:10 +0900

    update : refactoring 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 // import
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
-// theme
-import { system } from './theme/theme.ts'
-// others
-import { LoginUserProvider } from './providers/LoginUserProvider.tsx'
-import { TodoProvider } from './providers/TodoProvider.tsx'
-import { Router } from './router/Router.tsx'
+import { AppProviders } from './providers/AppProviders'
+import { Router } from './router/Router'
 
 const App = () => {
   return (
-    <>
-      <ChakraProvider value={system}>
-        <BrowserRouter>
-          <LoginUserProvider>
-            <TodoProvider>
-              <Router />
-            </TodoProvider>
-          </LoginUserProvider>
-        </BrowserRouter>
-        <Toaster />
-      </ChakraProvider>
-    </>
+    <AppProviders>
+      <Router />
+      <Toaster />
+    </AppProviders>
   )
 }
 
